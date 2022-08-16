f89ca21939d796b794ba6b9c3bd03b03c454de1d
Abigail <abibigailk@gmail.com>
2022-08-16 16:44:23 -0500

    java 1

---
+++ java/java-01/prueba2/src/main/java/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    public static void main(String[] args){
+
+        String nombre ="Abi uwu ";
+        String miVariable="Hola Mundo";
+        System.out.println("Saludos a : " + nombre);
+        System.out.println(miVariable);
+
+    }
+
+}
