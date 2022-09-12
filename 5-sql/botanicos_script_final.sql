7f79765f7e55fba7566bf5b18ba787359333a383
Abigail <abibigailk@gmail.com>
2022-09-12 16:54:06 -0500

    spring

--- 5-sql/botanicos_script_final.sql
+++ 5-sql/botanicos_script_final.sql
@@ -1,7 +1,7 @@
 -- creacion de la base de datos
 CREATE DATABASE botanicos;
 USE botanicos;
--- DROP DATABASE botanicos;
+
 
 -- creacion de las tablas
 CREATE TABLE cliente(
@@ -9,7 +9,8 @@ id_cliente  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(250) NOT NULL,
 correo VARCHAR(250) NOT NULL, 
 telefono VARCHAR(250) NOT NULL,
-tipo_cliente VARCHAR(50));
+tipo_cliente VARCHAR(250) NOT NULL
+);
 
 CREATE TABLE categoria(
 id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -32,15 +33,12 @@ CREATE TABLE orden_compra(
 id_compra INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 id_cliente  INT NOT NULL, 
 id_producto  INT NOT NULL, 
-cantidad_productos INT NOT NULL, 
 direccion VARCHAR(250) NOT NULL,
 fecha_compra DATE NOT NULL,
 fecha_entrega DATE NOT NULL,
 total DECIMAL(5,2) NOT NULL,
 foreign key(id_cliente)
-REFERENCES cliente(id_cliente),
-foreign key(id_producto)
-REFERENCES productos(id_producto)
+REFERENCES cliente(id_cliente)
 );
 
 CREATE TABLE proveedor(
@@ -54,5 +52,16 @@ foreign key(id_producto)
 REFERENCES productos(id_producto)
 );
 
-
-
+CREATE TABLE producto_orden_compra (
+  `productos_id_producto` INT NOT NULL,
+  `orden_compra_id_compra` INT NOT NULL,
+  PRIMARY KEY (`productos_id_producto`, `orden_compra_id_compra`),
+  INDEX `fk_productos_has_orden_compra_orden_compra1_idx` (`orden_compra_id_compra` ASC) VISIBLE,
+  INDEX `fk_productos_has_orden_compra_productos1_idx` (`productos_id_producto` ASC) VISIBLE,
+  CONSTRAINT `fk_productos_has_orden_compra_productos1`
+    FOREIGN KEY (`productos_id_producto`)
+    REFERENCES `botanicos`.`productos` (`id_producto`),    
+  CONSTRAINT `fk_productos_has_orden_compra_orden_compra1`
+    FOREIGN KEY (`orden_compra_id_compra`)
+    REFERENCES `botanicos`.`orden_compra` (`id_compra`)
+    );
